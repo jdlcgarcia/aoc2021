03978cd5c958e2e9b6772fb3bb2f8e375dcb1457
Juan Garcia <jcruzgar@gmail.com>
2021-12-03 14:22:18 +0100

    Day 02: Script that solves the second part

--- commands/02.php
+++ commands/02.php
@@ -3,13 +3,17 @@
 require_once 'vendor/autoload.php';
 
 use Jdlcgarcia\Aoc2021\Submarine\Position;
+use Jdlcgarcia\Aoc2021\Submarine\PositionWithAim;
 use Jdlcgarcia\Aoc2021\Submarine\Services\MoveService;
 use Jdlcgarcia\Aoc2021\Utils\FileParser;
 
 $parser = new FileParser();
 $movementsList = $parser->loadListFile('input/02.txt');
 $service = new MoveService(new Position());
+$serviceWithAim = new MoveService(new PositionWithAim());
 foreach($movementsList as $movement) {
     $service->processMove($movement);
+    $serviceWithAim->processMove($movement);
 }
 echo $service->getScalarPosition() . PHP_EOL;
+echo $serviceWithAim->getScalarPosition() . PHP_EOL;
