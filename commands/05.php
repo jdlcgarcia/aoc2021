d871be82773d8a9f8735b6f25205f96369d74550
Juan Garcia <jcruzgar@gmail.com>
2021-12-07 23:55:34 +0100

    Day 05: input and script

---
+++ commands/05.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Jdlcgarcia\Aoc2021\Submarine\DiagnosticReport;
+use Jdlcgarcia\Aoc2021\Submarine\HydrotermalCloud;
+use Jdlcgarcia\Aoc2021\Submarine\Point;
+use Jdlcgarcia\Aoc2021\Submarine\Services\HydrotermalRadar;
+use Jdlcgarcia\Aoc2021\Utils\FileParser;
+
+$parser = new FileParser();
+$rawHydrotermalClouds = $parser->loadListFile('input/05.txt');
+$hydrotermalClouds = [];
+foreach($rawHydrotermalClouds as $rawHydrotermalCloud) {
+    if ($rawHydrotermalCloud !== '') {
+        list($origin, $end) = explode(' -> ', $rawHydrotermalCloud);
+        list($x1, $y1) = explode(',', $origin);
+        list($x2, $y2) = explode(',', $end);
+        $hydrotermalClouds[] = new HydrotermalCloud(new Point($x1, $y1), new Point($x2, $y2));
+    }
+}
+
+$service = new HydrotermalRadar($hydrotermalClouds);
+echo $service->getOverlappingPositions() . PHP_EOL;
