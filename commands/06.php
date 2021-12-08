12a8bfda2a46e050c9a9be42b5c797ba593ef43d
Juan Garcia <jcruzgar@gmail.com>
2021-12-08 04:21:34 +0100

    Day 06: modified script to get the second solution

--- commands/06.php
+++ commands/06.php
@@ -2,16 +2,13 @@
 
 require_once 'vendor/autoload.php';
 
-use Jdlcgarcia\Aoc2021\Fish\Lanternfish;
-use Jdlcgarcia\Aoc2021\Fish\LanternfishSchool;
+use Jdlcgarcia\Aoc2021\Submarine\LanternfishSchool;
 use Jdlcgarcia\Aoc2021\Utils\FileParser;
 
 $parser = new FileParser();
 $lanternfishTimers = $parser->loadIntegerListFile('input/06.txt', ',');
-$membersOfSchool = [];
-foreach ($lanternfishTimers as $timer) {
-    $membersOfSchool[] = new Lanternfish($timer);
-}
-$school = new LanternfishSchool($membersOfSchool);
+$school = new LanternfishSchool($lanternfishTimers);
 $school->fastForward(80);
-echo count($school->getSchool()) . PHP_EOL;
+echo $school->getSchoolSize() . PHP_EOL;
+$school->fastForward(176);
+echo $school->getSchoolSize() . PHP_EOL;
