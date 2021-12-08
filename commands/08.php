0595e6ebd27cdd9efadc3fa16c7ab54880df62fa
Juan Garcia <jcruzgar@gmail.com>
2021-12-08 12:57:09 +0100

    Day 08: script to get solution and input

---
+++ commands/08.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Jdlcgarcia\Aoc2021\Submarine\CrabSubmarine;
+use Jdlcgarcia\Aoc2021\Submarine\Services\CrabSubmarineOptimizer;
+use Jdlcgarcia\Aoc2021\Submarine\SevenSegmentDisplay;
+use Jdlcgarcia\Aoc2021\Submarine\SevenSegmentNote;
+use Jdlcgarcia\Aoc2021\Utils\FileParser;
+
+$parser = new FileParser();
+$notes = $parser->loadListFile('input/08.txt');
+$sevenSegmentNotes = [];
+foreach($notes as $note) {
+    if (!empty($note)) {
+        $sevenSegmentNotes[] = new SevenSegmentNote($note);
+    }
+}
+$display = new SevenSegmentDisplay($sevenSegmentNotes);
+$display->processRightValues();
+echo $display->getUniqueSegmentCombinationCounter() . PHP_EOL;
