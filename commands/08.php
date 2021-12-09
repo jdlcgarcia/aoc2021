dc3f1cc89365872cea21a02a5d5c2eddc08d6b74
Juan Garcia <jcruzgar@gmail.com>
2021-12-09 13:46:35 +0100

    Day 08: adapted command to the second part

--- commands/08.php
+++ commands/08.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 
 use Jdlcgarcia\Aoc2021\Submarine\CrabSubmarine;
 use Jdlcgarcia\Aoc2021\Submarine\Services\CrabSubmarineOptimizer;
+use Jdlcgarcia\Aoc2021\Submarine\Services\SevenSegmentGuesser;
 use Jdlcgarcia\Aoc2021\Submarine\SevenSegmentDisplay;
 use Jdlcgarcia\Aoc2021\Submarine\SevenSegmentNote;
 use Jdlcgarcia\Aoc2021\Utils\FileParser;
@@ -19,3 +20,11 @@ foreach($notes as $note) {
 $display = new SevenSegmentDisplay($sevenSegmentNotes);
 $display->processRightValues();
 echo $display->getUniqueSegmentCombinationCounter() . PHP_EOL;
+$sum = 0;
+foreach($notes as $line) {
+    if (!empty($line)) {
+        $guesser = new SevenSegmentGuesser($line);
+        $sum += $guesser->guess();
+    }
+}
+echo $sum . PHP_EOL;
